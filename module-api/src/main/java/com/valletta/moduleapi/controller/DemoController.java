5ef92a2a0edacf99f7bc94bbf95d665cc05eaf4f
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-23 17:12:28 +0900

    module-api와 module-common 간의 의존성 설정

---
+++ module-api/src/main/java/com/valletta/moduleapi/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.valletta.moduleapi.controller;
+
+import com.valletta.moduleapi.service.DemoService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DemoController {
+
+    private final DemoService demoService;
+
+    @GetMapping("/save")
+    public String save() {
+        return demoService.save();
+    }
+
+    @GetMapping("/find")
+    public String find() {
+        return demoService.find();
+    }
+}
