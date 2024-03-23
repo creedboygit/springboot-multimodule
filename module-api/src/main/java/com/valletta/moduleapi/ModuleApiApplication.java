35f3b2511332cbca221c19f6f776b9d6fb39329c
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-23 20:19:23 +0900

    repository 설정

--- module-api/src/main/java/com/valletta/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/valletta/moduleapi/ModuleApiApplication.java
@@ -2,10 +2,15 @@ package com.valletta.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@EntityScan("com.valletta.modulecommon")
 @SpringBootApplication(
     scanBasePackages = {"com.valletta.moduleapi", "com.valletta.modulecommon"}
 )
+@EntityScan("com.valletta.modulecommon.domain")
+@EnableJpaRepositories(basePackages = "com.valletta.modulecommon.repository")
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
--- module-api/src/main/java/com/valletta/moduleapi/service/DemoService.java
+++ module-api/src/main/java/com/valletta/moduleapi/service/DemoService.java
@@ -1,23 +1,33 @@
 package com.valletta.moduleapi.service;
 
-import com.valletta.modulecommon.enums.CodeEnum;
+import com.valletta.modulecommon.domain.Member;
+import com.valletta.modulecommon.repository.MemberRepository;
 import com.valletta.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DemoService {
 
     private final CommonDemoService commonDemoService;
+    private final MemberRepository memberRepository;
 
     public String save() {
-        System.out.println(CodeEnum.SUCCESS.getCode());
-        System.out.println(commonDemoService.commonService());
+        memberRepository.save(Member.builder()
+//            .name("테스트")
+            .name(Thread.currentThread().getName())
+            .build());
+//        System.out.println(CodeEnum.SUCCESS.getCode());
+//        System.out.println(commonDemoService.commonService());
         return "save";
     }
 
     public String find() {
+        int size = memberRepository.findAll().size();
+        log.info("# DB Size: {}", size);
         return "find";
     }
 
